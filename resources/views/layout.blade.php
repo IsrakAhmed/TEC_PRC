b078b89b7d712f72cc2fe10b442ffe95b2461a1d
IsrakAhmed <israkahmed7@gmail.com>
2024-01-30 20:38:17 +0600

    logo bug solved

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -28,7 +28,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
     <a class="navbar-brand" style="width: 5%;" href="/">
-        <img style=" margin: -12px; margin-left: 1em;" src="{{ asset('storage/image/logo.jpeg') }}">
+        <img style="width: 100%; margin: -12px; margin-left: 1em;" src="{{ asset('storage/image/logo.jpeg') }}">
     </a>
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
